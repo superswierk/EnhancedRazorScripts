a52821fa2e0df289ab908910ccfabf815341d5e6
swierq <superswierk@gmail.com>
2025-06-16 23:34:53 +0200

    added helper scripts for stealing

--- misc_PutToContainer.py
+++ misc_PutToContainer.py
@@ -8,8 +8,8 @@ container = Items.FindBySerial(containerObj)
 itemToMoveID = 0x1766
 
 
-for item in container.Contains:
+for item in Player.Backpack.Contains:
     if item.ItemID == itemToMoveID:
-        Items.Move(item.Serial,Player.Backpack.Serial,1)
+        Items.Move(item.Serial,containerObj,1)
         Misc.Pause( 1000 )
 
--- train_Stealing.py
+++ train_Stealing.py
@@ -1,6 +1,33 @@
 from Scripts.EnhancedRazorScripts.misc_Discord import *
 import sys
 
+musicId = 0x0E9E
+# Helper Functions
+###################################
+def getByItemID(itemid, source):
+    #find an item id in container serial
+    searchItem = Items.FindBySerial(source)
+    if hasattr(searchItem,'Contains'):
+        for item in searchItem.Contains:
+            if item.ItemID == itemid:
+                return item
+            else:
+                Misc.NoOperation()
+    else:
+        Misc.NoOperation() 
+###################################
+
+
+def playMusic():
+    global musicId
+    playItem = getByItemID(musicId, Player.Backpack.Serial)
+    Misc.Pause(400)
+    if playItem is not None:
+        Items.UseItem(playItem)
+        print('Gram muzyke!')
+    else:
+        print('nie gram muzyki cos nie tak!')
+        
 
 stealTarget = Target.PromptTarget( 'Select whom to steal' )
 
@@ -15,9 +42,21 @@ while True:
        Journal.Search("Ujawniles sie") or 
        Journal.Search("Za szybko")):
         if Journal.Search("Udalo Ci sie ukrasc") or Journal.Search("Za szybko"):
-            Misc.Pause(2000)
+            Misc.Pause(4000)
             Player.UseSkill('Ukrywanie')
-            Misc.Pause(36000)
+            Misc.Pause(4000)
+            playMusic()
+            Misc.Pause(12000)
+            Player.UseSkill('Ukrywanie')
+            Misc.Pause(4000)
+            playMusic()
+            Misc.Pause(12000)
+            Player.UseSkill('Ukrywanie')
+            Misc.Pause(4000)
+            playMusic()
+            Misc.Pause(12000)
+            Player.UseSkill('Ukrywanie')
+            Misc.Pause(8000)
         Journal.Clear()
         Misc.Pause(2000)
         Player.UseSkill('Okradanie')
