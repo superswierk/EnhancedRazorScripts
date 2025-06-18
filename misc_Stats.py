348f48ebcb2efc7c0cfe1d1fc6cc85a65ed2ad93
swierq <superswierk@gmail.com>
2025-06-18 16:43:36 +0200

    added gump to misc_Stat

--- misc_Stats.py
+++ misc_Stats.py
@@ -1,4 +1,6 @@
 from Scripts.EnhancedRazorScripts.misc_Discord import *
+from System.Collections.Generic import List
+from System import Byte, Int32
 import sys
 Round = lambda x, n: eval('"%.'+str(int(n))+'f" % '+repr(int(x)+round(float('.'+str(float(x)).split('.')[1]),n)))
 lumberThumb = "https://i.imgur.com/FAb0xg0.png"
@@ -8,7 +10,121 @@ enemyThumb = "https://i.imgur.com/YvbQw56.png"
 lvlupThumb = "https://i.imgur.com/j5rUy80.png"
 apoThumb = "https://i.imgur.com/eDQLGaI.png"
 
-skillName =  'Druciarstwo'
+setX = 125 
+setY = 125
+offsetLabelY = 20
+offsetRadioY = 45
+offsetButtonY = 170
+
+STATICSKILLS = {
+    "Druciarstwo": 11,
+    "Stolarstwo": 12,
+    "Okaradanie": 13,
+    "Zagladanie": 14,
+    "Gornictwo": 15,
+    "Wlamywanie": 16,
+    "Drwalstwo": 17,
+    "Tworzenie Lukow": 18,
+    "Rolnictwo": 19
+}
+
+
+
+def sendgump():
+    gd = Gumps.CreateGump(movable=True) 
+    
+    Gumps.AddPage(gd, 0)
+    Gumps.AddBackground(gd, 0, 0, 200, (STATICSKILLS.Count + 3) * 20 + offsetRadioY, 2620) 
+
+    iY = 0
+    Gumps.AddLabel(gd,15,iY + offsetLabelY,2407,'Wybierz skill do sledzenia:')
+
+    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,True,STATICSKILLS['Druciarstwo'])
+    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Druciarstwo')
+
+    iY = iY + 20
+    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Stolarstwo'])
+    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Stolarstwo')
+
+    iY = iY + 20
+    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Okaradanie'])
+    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Okaradanie')
+    
+    iY = iY + 20
+    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Zagladanie'])
+    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Zagladanie')
+    
+    iY = iY + 20
+    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Gornictwo'])
+    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Gornictwo')
+    
+    iY = iY + 20
+    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Wlamywanie'])
+    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Wlamywanie')
+    
+    iY = iY + 20
+    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Drwalstwo'])
+    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Drwalstwo')
+    
+    iY = iY + 20
+    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Tworzenie Lukow'])
+    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Tworzenie Lukow')
+    
+    iY = iY + 20
+    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Rolnictwo'])
+    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Rolnictwo')
+    
+    
+    iY = iY + 40
+    Gumps.AddButton(gd,120,iY + offsetRadioY,247,248,456,1,0)
+
+    Gumps.SendGump(696969, Player.Serial, setX, setY, gd.gumpDefinition, gd.gumpStrings)
+    buttoncheck()
+
+
+
+def buttoncheck():
+    global skillName
+    Gumps.WaitForGump(696969, 60000)
+    Gumps.CloseGump(696969)
+    gdata = Gumps.GetGumpData(696969)
+    switchList = gdata.switches
+    if switchList.Count >= 1:
+        if switchList[0] == STATICSKILLS['Druciarstwo']:
+            skillName =  'Druciarstwo'
+            print("Druciarstwo")
+        elif switchList[0] == STATICSKILLS['Stolarstwo']:
+            skillName =  'Stolarstwo'
+            print("Stolarstwo")
+        elif switchList[0] == STATICSKILLS['Okaradanie']:
+            skillName =  'Okaradanie'
+            print("Okaradanie")
+        elif switchList[0] == STATICSKILLS['Zagladanie']:
+            skillName =  'Zagladanie'
+            print("Zagladanie")
+        elif switchList[0] == STATICSKILLS['Gornictwo']:
+            skillName =  'Gornictwo'
+            print("Gornictwo")
+        elif switchList[0] == STATICSKILLS['Wlamywanie']:
+            skillName =  'Wlamywanie'
+            print("Wlamywanie")
+        elif switchList[0] == STATICSKILLS['Drwalstwo']:
+            skillName =  'Drwalstwo'
+            print("Drwalstwo")
+        elif switchList[0] == STATICSKILLS['Tworzenie Lukow']:
+            skillName =  'Tworzenie Lukow'
+            print("Tworzenie Lukow")
+        elif switchList[0] == STATICSKILLS['Rolnictwo']:
+            skillName =  'Rolnictwo'
+            print("Rolnictwo")
+        else:
+            print("Default value " + skillName)
+    else:
+        print("error nie zaznaczyles nic uzwyam domyslnego " + skillName)
+
+skillName = 'Druciarstwo'
+sendgump()
+Misc.Pause(2000)
 
 Journal.Clear()
 lvlStealSkill = Player.GetRealSkillValue(skillName)
