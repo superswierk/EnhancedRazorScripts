25a9c6a7f17521fe0a4da331f5480116c08866f3
swierq <superswierk@gmail.com>
2025-06-19 13:56:21 +0200

    drop items added prompt target

--- misc_DropItems.py
+++ misc_DropItems.py
@@ -1,12 +1,12 @@
 import sys
 
-#magazynier
-magazynID = 0x51B09F1E
+containerObj = Target.PromptTarget( 'Wybierz pojemnik z ktorego wywalic rzeczy' )
+container = Items.FindBySerial(containerObj)
 itemsToDrop = [0x09D7, 0x15F8,0x0FF6]
-dragDelay = 700
+dragDelay = 400
 
 def MoveToGround():
-    for item in Player.Backpack.Contains:
+    for item in container.Contains:
         for itemToDrop in itemsToDrop: 
             if item.ItemID == itemToDrop:
                 Items.DropItemGroundSelf(item,0)
