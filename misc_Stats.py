833ae9492f03389ae507e4f3312ea18c0c963de0
swierq <superswierk@gmail.com>
2025-06-29 02:22:24 +0200

    added zakradanie to stats

--- misc_Stats.py
+++ misc_Stats.py
@@ -1,6 +1,6 @@
 from Scripts.EnhancedRazorScripts.misc_Discord import *
 from System.Collections.Generic import List
-from System import Byte, Int32
+from System import Byte, Int32, Double
 import sys
 Round = lambda x, n: eval('"%.'+str(int(n))+'f" % '+repr(int(x)+round(float('.'+str(float(x)).split('.')[1]),n)))
 lumberThumb = "https://i.imgur.com/FAb0xg0.png"
@@ -25,7 +25,8 @@ STATICSKILLS = {
     "Wlamywanie": 16,
     "Drwalstwo": 17,
     "Tworzenie Lukow": 18,
-    "Rolnictwo": 19
+    "Rolnictwo": 19,
+    "Zakradanie": 20
 }
 
 
@@ -38,44 +39,20 @@ def sendgump():
 
     iY = 0
     Gumps.AddLabel(gd,15,iY + offsetLabelY,2407,'Wybierz skill do sledzenia:')
-
-    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,True,STATICSKILLS['Druciarstwo'])
-    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Druciarstwo')
-
-    iY = iY + 20
-    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Stolarstwo'])
-    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Stolarstwo')
-
-    iY = iY + 20
-    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Okaradanie'])
-    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Okaradanie')
+    
+    defaultSelected = 0
+    if Misc.CheckSharedValue("misc_StatsState") == True:
+        defaultSelected = Misc.ReadSharedValue("misc_StatsState")
+    
+    for skill in STATICSKILLS:
+        defultValue = False
+        if STATICSKILLS[skill] == defaultSelected:
+            defultValue = True
+        Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,defultValue,STATICSKILLS[skill])
+        Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,skill)
+        iY = iY + 20
     
     iY = iY + 20
-    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Zagladanie'])
-    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Zagladanie')
-    
-    iY = iY + 20
-    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Gornictwo'])
-    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Gornictwo')
-    
-    iY = iY + 20
-    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Wlamywanie'])
-    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Wlamywanie')
-    
-    iY = iY + 20
-    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Drwalstwo'])
-    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Drwalstwo')
-    
-    iY = iY + 20
-    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Tworzenie Lukow'])
-    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Tworzenie Lukow')
-    
-    iY = iY + 20
-    Gumps.AddRadio(gd,15,iY + offsetRadioY,209,208,False,STATICSKILLS['Rolnictwo'])
-    Gumps.AddLabel(gd,35,iY + offsetRadioY,2407,'Rolnictwo')
-    
-    
-    iY = iY + 40
     Gumps.AddButton(gd,120,iY + offsetRadioY,247,248,456,1,0)
 
     Gumps.SendGump(696969, Player.Serial, setX, setY, gd.gumpDefinition, gd.gumpStrings)
@@ -90,34 +67,13 @@ def buttoncheck():
     gdata = Gumps.GetGumpData(696969)
     switchList = gdata.switches
     if switchList.Count >= 1:
-        if switchList[0] == STATICSKILLS['Druciarstwo']:
-            skillName =  'Druciarstwo'
-            print("Druciarstwo")
-        elif switchList[0] == STATICSKILLS['Stolarstwo']:
-            skillName =  'Stolarstwo'
-            print("Stolarstwo")
-        elif switchList[0] == STATICSKILLS['Okaradanie']:
-            skillName =  'Okaradanie'
-            print("Okaradanie")
-        elif switchList[0] == STATICSKILLS['Zagladanie']:
-            skillName =  'Zagladanie'
-            print("Zagladanie")
-        elif switchList[0] == STATICSKILLS['Gornictwo']:
-            skillName =  'Gornictwo'
-            print("Gornictwo")
-        elif switchList[0] == STATICSKILLS['Wlamywanie']:
-            skillName =  'Wlamywanie'
-            print("Wlamywanie")
-        elif switchList[0] == STATICSKILLS['Drwalstwo']:
-            skillName =  'Drwalstwo'
-            print("Drwalstwo")
-        elif switchList[0] == STATICSKILLS['Tworzenie Lukow']:
-            skillName =  'Tworzenie Lukow'
-            print("Tworzenie Lukow")
-        elif switchList[0] == STATICSKILLS['Rolnictwo']:
-            skillName =  'Rolnictwo'
-            print("Rolnictwo")
-        else:
+        Misc.SetSharedValue("misc_StatsState",switchList[0])
+        for skill in STATICSKILLS:   
+            if switchList[0] == STATICSKILLS[skill]:
+                skillName =  skill
+                print(f"{skill}")
+        if skillName is None:
+            skillName = 'Druciarstwo'
             print("Default value " + skillName)
     else:
         print("error nie zaznaczyles nic uzwyam domyslnego " + skillName)
@@ -127,8 +83,8 @@ sendgump()
 Misc.Pause(2000)
 
 Journal.Clear()
-lvlStealSkill = Player.GetRealSkillValue(skillName)
-lvlStealSkillNew = Player.GetRealSkillValue(skillName)
+lvlStealSkill = float(Player.GetRealSkillValue(skillName))
+lvlStealSkillNew = float(Player.GetRealSkillValue(skillName))
 Player.ChatSay('.glod wszystko')
 Misc.Pause(3000)
 glod = Journal.GetLineText('Glod')
@@ -151,10 +107,11 @@ while True:
         apokalipseStr = Journal.GetLineText('Apokalipsa')
         apocalipse = True
         sendDiscord("Uwaga:\n" + apokalipseStr + "\n", 14696255, apoThumb)
-    lvlStealSkillNew = Player.GetRealSkillValue(skillName)
-    if lvlStealSkill != lvlStealSkillNew:
-        lvlStealSkill = lvlStealSkillNew
-        sendDiscord("Wzrost umiejetnosci " + skillName + " masz teraz: " + str(Round(lvlStealSkill,1)), 5814783, lvlupThumb)
+    lvlStealSkillNew = float(Player.GetRealSkillValue(skillName))
+    if lvlStealSkillNew > float(50) or int(lvlStealSkillNew) == lvlStealSkillNew:
+        if lvlStealSkill != lvlStealSkillNew:
+            lvlStealSkill = lvlStealSkillNew
+            sendDiscord("Wzrost umiejetnosci " + skillName + " masz teraz: " + str(Round(lvlStealSkill,1)), 5814783, lvlupThumb)
     if(Timer.Check('eatingLogTimer') == False):
         Timer.Create('eatingLogTimer', 120000)
         Player.ChatSay('.glod wszystko')
