77d29ee247c74cd09d785513873333c926496fec
swierq <superswierk@gmail.com>
2025-07-05 11:25:40 +0200

    discord send added to guard alert

--- misc_GuardAlert.py
+++ misc_GuardAlert.py
@@ -1,7 +1,13 @@
+from Scripts.EnhancedRazorScripts.misc_Discord import *
+enemyThumb = "https://i.imgur.com/YvbQw56.png"
+Journal.Clear()
 while True:
     enemy = Target.GetTargetFromList( 'enemywar' )
-    if Journal.Search("Przyciagnales uwage",) or Journal.Search("podchodzi zobaczyc co ciekawego") or enemy != None:
+    if Journal.Search("ent: ent") or Journal.Search("Przyciagnales uwage") or Journal.Search("podchodzi zobaczyc co ciekawego") or enemy != None:
+        if Journal.Search("ent: ent"):
+            sendDiscord("Uwaga wrog w popblizu!", 15291726, enemyThumb)
         Journal.Clear()
+        
         Player.ChatSay("STRAZE POMOCY BIJA MNIE")
         Misc.Pause(2000)
         Player.ChatSay("STRAZE POMOCY BIJA MNIE!")
